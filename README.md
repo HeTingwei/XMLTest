9e420f7daf191d02b0459387e2df651502f21839
HeTingwei <2249382610@qq.com>
2017-09-17 20:30:21 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+#XMLTest
+有两个module，a_man是lay-list练手,形成一个人的形状，app是shape和lay-list的学习笔记实例
---
+++ app/src/main/java/com/xingyi/xmltest/MainActivity.java
@@ -0,0 +1,18 @@
+package com.xingyi.xmltest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+/*
+*
+*   drawable文件夹下xml文件使用方法
+* */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
